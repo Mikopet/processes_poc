218f6896f9c020270a2702bfce66ace0260687e7
Peter MIKOLA <mikopet@mikopet.dev>
2024-10-17 01:04:15 +0200

    :heavy_minus_sign: remove `backtrace` and simplify logging

--- src/logger.rs
+++ src/logger.rs
@@ -1,12 +1,7 @@
-use std::{fmt::Arguments, sync::LazyLock};
-
 use colored::{Color, ColoredString, Colorize};
 use log::*;
 
-use crate::Kind;
-
 static LOGGER: Logger = Logger;
-static COLOR: LazyLock<Color> = LazyLock::new(|| Color::from(Kind::current()));
 
 pub fn init() -> Result<(), SetLoggerError> {
     log::set_logger(&LOGGER).map(|()| log::set_max_level(LevelFilter::Info))
@@ -15,64 +10,45 @@ pub fn init() -> Result<(), SetLoggerError> {
 pub struct Logger;
 
 impl Logger {
-    fn deliver(m: &Metadata, a: &Arguments) {
-        println!(
-            "{}",
-            Self::decorate(
-                a.to_string(),
-                match m.level() {
-                    Level::Error => Color::Red,
-                    _ => *COLOR,
-                }
-            )
+    fn deliver(m: &Metadata, a: &std::fmt::Arguments) {
+        println!("{}", Self::decorate(a.to_string(), m.level(),));
+    }
+
+    fn decorate(msg: String, l: Level) -> String {
+        let process = format!(
+            "{:>7}#{:<6}",
+            crate::Kind::current().to_string(),
+            std::process::id()
         );
+        let prefix = format!(
+            "{} {} ",
+            process.bright_black(),
+            Self::delimiter(l, msg.contains('\n'))
+        );
+
+        let msg = msg.replace('\n', format!("\n{prefix}").as_str());
+
+        format!("{prefix}{}", msg.color(Self::color(l)))
     }
 
-    fn caller() -> String {
-        let mut sym = String::from("");
-        backtrace::trace(|frame| {
-            backtrace::resolve_frame(frame, |symbol| {
-                if let Some(name) = symbol.name() {
-                    sym = name.to_string();
-                }
-            });
-
-            if sym.contains("processes_poc") {
-                if !sym.contains("logger") {
-                    sym = match *sym.split("::").collect::<Vec<&str>>() {
-                        [_, m, f, _] => format!("{m}::{f}()"),
-                        [_, f, _] => format!("main::{f}()"),
-                        _ => String::from("unknown"),
-                    };
-                    return false;
-                }
-            }
-
-            true // keep going to the next frame
-        });
-        sym
-    }
-
-    fn task() -> &'static str {
-        if Self::caller().contains("task") {
-            "="
-        } else {
-            " "
+    fn color(l: Level) -> Color {
+        match l {
+            Level::Error => Color::BrightRed,
+            Level::Warn => Color::BrightYellow,
+            Level::Info => Color::BrightWhite,
+            Level::Debug => Color::BrightCyan,
+            Level::Trace => Color::BrightMagenta,
         }
     }
 
-    fn kind() -> String {
-        format!("[{}#{}]", Kind::current().to_string(), std::process::id())
-    }
-
-    fn decorate(msg: String, c: Color) -> ColoredString {
-        format!(
-            "{:>16} | {:<13} |{} {msg}",
-            Self::kind(),
-            Self::caller(),
-            Self::task(),
-        )
-        .color(c)
+    fn delimiter(l: Level, m: bool) -> ColoredString {
+        match l {
+            Level::Error => "!".red().bold(),
+            _ => match m {
+                false => "|".white().bold(),
+                _ => "»".white(),
+            },
+        }
     }
 }
 
